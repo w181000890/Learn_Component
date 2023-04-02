fa64d6d299c25039365721a18a44ee65eae866d3
meng <s81000890@hnu.edu.cn>
2023-04-02 22:22:44 +0800

    1

--- 03_product_vite/src/App.vue
+++ 03_product_vite/src/App.vue
@@ -1,10 +1,17 @@
 <template>
-  
+  <div>
+    <h1>我是App</h1>
+    <productItem></productItem>
+  </div>
 </template>
 
 <script>
+import productItem from './components/productItem.vue';
 export default {
-
+      name:"App",
+      components:{
+        productItem
+      }
 }
 </script>
 
