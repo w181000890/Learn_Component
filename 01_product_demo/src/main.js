c538866162e515a430f536b8f5b1eb893e8c8b6b
meng <s81000890@hnu.edu.cn>
2023-04-02 08:25:37 +0800

    1

--- 01_product_demo/src/main.js
+++ 01_product_demo/src/main.js
@@ -1,4 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue/dist/vue.esm-bundler'
 
+const App = {
+  template:'<h2>{{content}}</h2>',
+  data() {
+    return {
+      content:"我是标题"
+    }
+  },
+}
 createApp(App).mount('#app')
