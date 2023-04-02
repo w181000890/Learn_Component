032978bc5fca2819c3d05c8b32541a8da89a4dd3
meng <s81000890@hnu.edu.cn>
2023-04-02 10:54:10 +0800

    1

--- 01_product_demo/src/main.js
+++ 01_product_demo/src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './components/App'
 // import ProductItem from "./ProductItem"
+import './utils/abc/compu'
+
 
 const app = createApp(App)
 // 全局注册
--- 01_product_demo/vue.config.js
+++ 01_product_demo/vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack:{
+    resolve:{
+      //p配置路径别名
+      // @对应的是src
+      alias:{
+        "utils":"@/utils"
+      }
+    }
+  }
 })
